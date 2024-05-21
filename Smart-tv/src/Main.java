6e2f088581974f17a2e21b13c54aa91d199987f0
debora <deborapatriciadp27@gmail.com>
2024-05-21 15:45:01 -0300

    fit: projeto Smart TV, ligar, canal e volume

---
+++ Smart-tv/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        /* Liga/Desliga */
+        SmartTv smartTv = new SmartTv();
+
+        String mensagemTv = smartTv.ligarTv(true);
+        if (!mensagemTv.isEmpty()) {
+            System.out.println(mensagemTv);
+        }
+
+        String mensagemDoCanal = smartTv.mudarCanal(null);
+        if (!mensagemDoCanal.isEmpty()) {
+            System.out.println(mensagemDoCanal);
+        }
+
+        String mensagemVolume = smartTv.definirVolume(15);
+        System.out.println(mensagemVolume);
+
+        
+        String mensagemDiminuirVolume = smartTv.diminuirVolume(0);
+        System.out.println(mensagemDiminuirVolume);
+        String mensagemAumentarVolume = smartTv.aumentarVolume(20);
+        System.out.println(mensagemAumentarVolume);
+    }
+}
---
+++ Smart-tv/src/SmartTv.java
@@ -0,0 +1,66 @@
+public class SmartTv {
+  boolean ligada = false;
+  boolean primeiraVezLigada = true;
+  int canal = 1;
+  int volume = 10;
+
+  public String ligarTv(boolean ligar) {
+    if (ligar && !ligada) {
+      ligada = true;
+      if (primeiraVezLigada) {
+        primeiraVezLigada = false;
+        return "TV foi ligada";
+      }
+    } else {
+      ligada = false;
+      primeiraVezLigada = true;
+      return "TV foi desligada";
+    }
+    return "";
+  }
+
+  public String mudarCanal(Integer novoCanal) {
+    if (ligada) {
+      if (novoCanal != null && novoCanal != canal) {
+        canal = novoCanal;
+        return "Mudou canal " + canal;
+      }
+      return "Canal " + canal;
+    } else {
+      return "";
+    }
+  }
+
+  public String definirVolume(int novoVolume) {
+    if (ligada && novoVolume >= 0 && novoVolume <= 100) {
+      volume = novoVolume;
+      return "Mudou Volume para " + volume;
+    } else {
+      return "Volume inválido ou TV desligada";
+    }
+  }
+
+  public String aumentarVolume(int aumentaVolume) {
+    if (ligada && volume <= 100) {
+      while (volume < aumentaVolume) {
+        volume++;
+        System.out.println("Aumentar Volume " + volume);
+      }
+      return "";
+    } else {
+      return "Volume maximo atingido";
+    }
+  }
+
+  public String diminuirVolume(int diminuirVolume) {
+    if (ligada && volume >= 0 && volume > diminuirVolume) {
+      while (volume > diminuirVolume) {
+        volume--;
+        System.out.println("Diminuir Volume " + volume);
+      }
+      return "";
+    } else {
+      return "Volume mínimo atingido";
+    }
+  }
+}
